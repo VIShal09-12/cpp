45cd8f8870dd1bda2fda9498ef9f0383cb9c1db2
Vishal <mandalvishal208@gmail.com>
2023-08-18 14:40:48 +0530

    18 aug

---
+++ Example.java
@@ -0,0 +1,12 @@
+
+import java.util.*;
+
+public class Example{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        boolean arr[] = new boolean[5];
+        // Boolean array when created by default have 'false' value assigned to them
+        System.out.println(arr[0]);
+        sc.close();
+    }
+}
