2ac61adfba4ac1d1fdc8eb1ee1bf33aa1184c423
Vishal <mandalvishal208@gmail.com>
2023-08-28 14:45:25 +0530

    28 aug

--- loops.java
+++ loops.java
@@ -1,32 +1,32 @@
 
 // For loop:
 
-import java.util.*;
+// import java.util.*;
 
-public class loops {
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        for(int i=1; i<=33; i++){
-            System.out.println("hello bhav");
-        }
+// public class loops {
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner(System.in);
+//         for(int i=1; i<=33; i++){
+//             System.out.println("hello bhav");
+//         }
 
-    }
-}
+//     }
+// }
 
 // While loop:  jab tak
 
-// import java.util.*;
-// public class loops{
-//     public static void main(String args[]){
-//         Scanner sc = new Scanner(System.in);
-//         int i = 0;
-//         while(i<33){
-//             System.out.println("hello semcii");
-//             i++;
-//         }
-//         sc.close();
-//     }
-// }
+import java.util.*;
+public class loops{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int i = 0;
+        while(i<33){
+            System.out.println("hello semcii");
+            i++;
+        }
+        sc.close();
+    }
+}
 
 //  Do while: do the work until the condition is reached
 
