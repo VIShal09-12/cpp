d1ff324f9a24509e923f66746dd9d5cf81610245
Vishal <mandalvishal208@gmail.com>
2023-09-08 14:52:12 +0530

    8 sep

--- Patterns.java
+++ Patterns.java
@@ -10,15 +10,7 @@ public class Patterns {
         sc.close();
     }
 
-    public static void pattern1(int n){
-        for(int i=1; i<= n; i++){
-            for(int j=1; j<= n; j++){
-                System.out.print("*"+ " ");
-            }
-            System.out.println();
-        }
-    }
-
+    
     public static void pattern2(int n){
         for(int i= 1; i<= n; i++){
             for(int j= 1; j<= i; j++){
