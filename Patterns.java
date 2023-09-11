9f027446c20d071e921e9831160dd1e516145c7f
Vishal <mandalvishal208@gmail.com>
2023-09-11 14:52:46 +0530

    11 sep

--- Patterns.java
+++ Patterns.java
@@ -12,15 +12,6 @@ public class Patterns {
 
     
 
-    public static void pattern3(int n){
-        for(int i=1; i<= n; i++){
-            for(int j= n-i+1; j> 0; j--){
-                System.out.print("*" + " ");
-            }
-            System.out.println();
-        }
-    }
-
     public static void pattern4(int n){
         for(int i=1; i<= n; i++){
             for(int j=1; j<= i; j++){
