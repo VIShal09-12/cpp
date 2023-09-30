c123161ea2bfad030545000b8cb921ba593f581c
Vishal <mandalvishal208@gmail.com>
2023-09-30 15:00:35 +0530

    30 sep

--- TwoD_arrays.java
+++ TwoD_arrays.java
@@ -24,40 +24,40 @@ public class TwoD_arrays{
     }
 }
 
-import java.util.*;
+// import java.util.*;
 
-public class TwoD_arrays{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int rows = sc.nextInt();
-        int cols = sc.nextInt();
-        int arr[][]= new int[rows][cols];
-        boolean found= false;
+// public class TwoD_arrays{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int rows = sc.nextInt();
+//         int cols = sc.nextInt();
+//         int arr[][]= new int[rows][cols];
+//         boolean found= false;
 
-        for(int i=0; i<rows; i++){
-            for(int j=0; j<cols; j++){
-                arr[i][j]= sc.nextInt();
-            }
-        }
+//         for(int i=0; i<rows; i++){
+//             for(int j=0; j<cols; j++){
+//                 arr[i][j]= sc.nextInt();
+//             }
+//         }
 
-        System.out.print("enter the number you want to search for: ");
-        int num = sc.nextInt();
+//         System.out.print("enter the number you want to search for: ");
+//         int num = sc.nextInt();
 
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
-                if(arr[i][j]==num){
-                    System.out.println("The number u are searching is " + num + " and it is found at index "+ i +","+j);
-                    found= true;
-                }
-            }
-        }
+//         for(int i=0;i<rows;i++){
+//             for(int j=0;j<cols;j++){
+//                 if(arr[i][j]==num){
+//                     System.out.println("The number u are searching is " + num + " and it is found at index "+ i +","+j);
+//                     found= true;
+//                 }
+//             }
+//         }
 
-        if(!found){
-            System.out.println("The number is not present in the array");
-        }
-        sc.close();
-    }
-}
+//         if(!found){
+//             System.out.println("The number is not present in the array");
+//         }
+//         sc.close();
+//     }
+// }
 
 // import java.util.*;
 
